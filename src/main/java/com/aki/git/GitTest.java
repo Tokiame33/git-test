c19de0688209212a25687cdfaebae525b4ab9dd8
tokiame <1012376712@qq.com>
2021-07-11 17:48:33 +0800

    master commit2

--- src/main/java/com/aki/git/GitTest.java
+++ src/main/java/com/aki/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello GitTest");
         System.out.println("Hello GitTest hot-fix");
+        System.out.println("Hello GitTest2");
     }
 
 }
