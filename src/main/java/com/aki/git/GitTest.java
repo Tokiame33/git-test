fed40268d5c38a8a1f8a91f20cc64617af65d1b3
tokiame <1012376712@qq.com>
2021-07-11 22:03:29 +0800

    push commit

--- src/main/java/com/aki/git/GitTest.java
+++ src/main/java/com/aki/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("Hello GitTest hot-fix");
         System.out.println("Hello GitTest2");
         System.out.println("Hello GitTest hot-fix2");
+        System.out.println("Push test");
     }
 
 }
